b90664fd8b74abd184516233bc8520f9df57b15a
EnflowSoft India <enflowsoftindia@gmail.com>
2024-08-24 11:22:56 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

---
+++ modules/2fa/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navigator from "./navigator";
+import { slice } from "./store";
+
+const TwoFactorAuthentication = () => {
+  return <Navigator />;
+};
+
+export default {
+  title: "2FA",
+  navigator: TwoFactorAuthentication,
+  slice
+};
